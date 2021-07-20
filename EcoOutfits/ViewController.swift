09a6c50a5360f5dd8885b11cb78078b0b13efb6f
Katherine Dong <dongkatherine3@gmail.com>
2021-07-20 14:26:49 -0400

    Initial Commit

---
+++ EcoOutfits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EcoOutfits
+//
+//  Created by Katherine Dong on 7/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
